8e0083407cf2685d70c7784ca108183c45006116
Santhosh Annamalai <34129975+Santhosh-Annamalai@users.noreply.github.com>
2023-10-25 03:12:23 +0530

    Changes

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-import { WebSocketServer } from "ws";
-const wss = new WebSocketServer({ port: 8080, clientTracking: true }, console.log("Running on port 8080"));
+const express = require('express');
+const app = express();
+// For parsing application/json
+app.use(express.json());
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+const PORT = 3000;
 const playerList = [];
+const matchedList = [];
 
-wss.on("connection", function connection(ws) {
-  ws.on("message", function message(data) {
-    if (playerList.length === 0) {
-        playerList.push([ws, data]);
-        ws.send("Kindly wait for a match");
+app.listen(PORT, (error) => {
+    if (!error) {
+        console.log("Server is Successfully Running, and App is listening on port " + PORT);
     }
-    else if (playerList.length === 1) {
-        console.log("" + data + " got matched with " + playerList[0][1]);
-        ws.send("You got matched with " + playerList[0][1]);
-        playerList[0][0].send("You got matched with " + data);
-        playerList.splice(0, 1);
+    else {
+        console.log("Error occurred, server can't start", error);
     }
-  });
+  }
+);
 
-  ws.send("Connected to Game Server!");
+app.get('/', (req, res) => {
+  const pair = matchedList.filter(match => (match[0] === req.query.clientID));
+  const filterCheck = playerList.filter(player => (player.clientID === req.query.clientID));
+  if ((playerList.length === 0) && (pair.length === 0)) {
+    playerList.push(req.query);
+    res.send("Kindly wait for a match");
+  }
+  else if ((playerList.length === 1) && (filterCheck.length === 0) && (pair.length === 0)) {
+    console.log("" + req.query.clientName + " got matched with " + playerList[0].clientName);
+    res.send("You got matched with " + playerList[0].clientName);
+    matchedList.push([playerList[0].clientID, req.query.clientID, req.query.clientName]);
+    playerList.splice(0, 1);
+  }
+  else if (pair.length === 1) {
+    res.send("You got matched with " + pair[0][2]);
+    matchedList.splice(0, 1);
+    pair.splice(0, 1);
+  }
+  else if ((playerList.length === 1) && (filterCheck.length === 1)) {
+    res.send("Kindly wait for a match");
+  }
+  res.status(200);
 });
