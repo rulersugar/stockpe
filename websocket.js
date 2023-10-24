42d68b7401e16ef24a96dd0ed75e0539ca265d01
Santhosh Annamalai <34129975+Santhosh-Annamalai@users.noreply.github.com>
2023-10-25 03:13:00 +0530

    Delete websocket.js

--- websocket.js
+++
@@ -1,8 +0,0 @@
-const webSocket = new self.WebSocket("wss://c17c-2409-40f4-11-9268-2116-e049-500f-cd72.ngrok-free.app");
-webSocket.onopen = (event) => {
-    webSocket.send("Riya");
-};
-webSocket.onmessage = (event) => {
-    console.log(event.data);
-    alert(event.data);
-};
