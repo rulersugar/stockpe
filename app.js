834329732b066496e1b9b0cde0db411d8e732e19
Santhosh Annamalai <34129975+Santhosh-Annamalai@users.noreply.github.com>
2023-10-25 20:16:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // For parsing application/json
 app.use(express.json());
@@ -7,6 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = 3000;
 const playerList = [];
 const matchedList = [];
+const gamersList = [];
 
 app.listen(PORT, (error) => {
     if (!error) {
@@ -18,26 +19,27 @@ app.listen(PORT, (error) => {
   }
 );
 
-app.get('/', (req, res) => {
+app.get("/match", (req, res) => {
   const pair = matchedList.filter(match => (match[0] === req.query.clientID));
   const filterCheck = playerList.filter(player => (player.clientID === req.query.clientID));
   if ((playerList.length === 0) && (pair.length === 0)) {
     playerList.push(req.query);
-    res.send("Kindly wait for a match");
+    res.send({ status: "waiting" });
   }
   else if ((playerList.length === 1) && (filterCheck.length === 0) && (pair.length === 0)) {
     console.log("" + req.query.clientName + " got matched with " + playerList[0].clientName);
-    res.send("You got matched with " + playerList[0].clientName);
-    matchedList.push([playerList[0].clientID, req.query.clientID, req.query.clientName]);
+    res.send({ status: "matched", clientID: playerList[0].clientID, clientName: playerList[0].clientName });
+    matchedList.push([playerList[0].clientID, req.query.clientID, req.query.clientName, playerList[0].clientName]);
     playerList.splice(0, 1);
   }
   else if (pair.length === 1) {
-    res.send("You got matched with " + pair[0][2]);
+    res.send({ status: "matched", clientID: pair[0][1], clientName: pair[0][2] });
     matchedList.splice(0, 1);
+    gamersList.push(pair);
     pair.splice(0, 1);
   }
   else if ((playerList.length === 1) && (filterCheck.length === 1)) {
-    res.send("Kindly wait for a match");
+    res.send({ status: "waiting" });
   }
   res.status(200);
 });
